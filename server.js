0b0c800751dbae902670bdb81217bed645f5e1e1
System <none>
2017-10-16 02:57:02 +0000

    Exporting to https://github.com/ilvcs/URL-Shortener-Microservice.git

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var URLS = require('./urls.js');
-var dbUrl = "mongodb://fccdb:t12345@ds121575.mlab.com:21575/surls"
+var dbUrl = "mongodb://shortUrlDb:t123@ds121575.mlab.com:21575/surls"
 mongoose.Promise = require('bluebird');
 mongoose.connect(dbUrl);
 var db = mongoose.connection;
@@ -21,13 +21,13 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.send(request.params);
+  response.send("Type your url to shorten at the end of the url Ex:" + " https://rhinestone-jewel.glitch.me/http://www.yoururl.com");
 });
 
 //Our work goes here
 app.get("/http://:url", function (req, res) {
   var url = "http://" + req.params.url
-  console.log(url)
+  //console.log(url)
    var bpattren = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
    var spattren = /^[a-zA-Z0-9]{4}$/;
   if(url.match(bpattren)){
@@ -36,12 +36,12 @@ app.get("/http://:url", function (req, res) {
      var randomUrl = getRandomString(4)
     var shortUrl =  "https://rhinestone-jewel.glitch.me" +"/" + randomUrl 
      // save that as a key in the database
-    var url = new URLS({
+    var URL = new URLS({
        shortUrl: randomUrl,
        longUrl:String(url)
     })
     
-    url.save(function(err,data){
+    URL.save(function(err,data){
       if(error){
         console.log("error saving urls");
       }else{
@@ -51,20 +51,50 @@ app.get("/http://:url", function (req, res) {
         
       }
     })
-     
-      
-   }else if(url.match(spattren)){
-     // got the shortened url
-     // query the db to get the long url 
-     // redirect to the long url 
-     
-   }else{
+  }else{
      // got the the junk
      // send the error
      var error = {"error":"This url is not the valid url."}
      res.send(error);
    }
 });
+
+//Our work goes here
+app.get("/https://:url", function (req, res) {
+  var url = "https://" + req.params.url
+  //console.log(url)
+   var bpattren = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+   var spattren = /^[a-zA-Z0-9]{4}$/;
+  if(url.match(bpattren)){
+     // got the long url
+     // genarate a shortened word with 4 letters
+     var randomUrl = getRandomString(4)
+    var shortUrl =  "https://rhinestone-jewel.glitch.me" +"/" + randomUrl 
+     // save that as a key in the database
+    var URL = new URLS({
+       shortUrl: randomUrl,
+       longUrl:String(url)
+    })
+    
+    URL.save(function(err,data){
+      if(error){
+        console.log("error saving urls");
+      }else{
+        // and send that url as a response to the user
+        var urlToSend = {"original_url":url,"short_url":shortUrl}
+        res.send(urlToSend);
+        
+      }
+    })
+  }else{
+     // got the the junk
+     // send the error
+     var error = {"error":"This url is not the valid url."}
+     res.send(error);
+   }
+});
+
+
 
 app.get("/:url", function (req, res) {
   var url = req.params.url
@@ -74,7 +104,15 @@ app.get("/:url", function (req, res) {
      // got the shortened url
      // query the db to get the long url 
      // redirect to the long url 
-    res.send(url);
+    
+    URLS.findOne({shortUrl:url},function(err,data){
+      if(error){
+        res.send({"error":"No url in the database"});
+      }else{
+        res.redirect(data.longUrl);
+      }
+    })
+    
      
    }else{
      // got the the junk
